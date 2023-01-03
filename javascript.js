2b0f1065158327cb5cbb0cb66f292cf5ef75d527
aswithadigavanti <digavantiaswitha111@gmail.com>
2023-01-03 18:37:35 +0530

    add few lines of capgemini

--- javascript.js
+++ javascript.js
@@ -1,4 +1,11 @@
 <script>  
-var stringname=new String("hello javascript string");  
-document.write(stringname);  
+var stringname=new String("hello capgemini");  
+document.write(stringname); 
+var stringname=new String("organization");  
+document.write(stringname); 
+var stringname=new String("good");  
+document.write(stringname); 
+var stringname=new String("work");  
+document.write(stringname); 
 </script>  
+
